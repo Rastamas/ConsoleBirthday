4028148652f05ee5290be92cd4bf4c4debdcc73a
Tamás Szabó <r.tamas.szabo@gmail.com>
2020-12-19 12:08:00 +0100

    Implement birthday cake drawing

---
+++ DrawingExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBirthday
+{
+    public static class DrawingExtensions
+    {
+        private const char LineEnding = 'X';
+        private static readonly string LineEndingString = 'X'.ToString();
+
+        public static void WriteFrame(string frame)
+        {
+            var column = 0;
+            var row = 0;
+
+            foreach (var character in frame)
+            {
+                if (character == LineEnding)
+                {
+                    row++;
+                    column = 0;
+                    continue;
+                } else
+                {
+                    Console.SetCursorPosition(column++, row);
+                    Console.Write(character);
+                }
+            }
+        }
+
+        public static string[] ConvertFrames(string[] frames)
+        {
+            var convertedFrames = new string[frames.Length];
+            var i = 0;
+
+            foreach (var frame in frames)
+            {
+                convertedFrames[i++] = frame.Replace(Environment.NewLine, LineEndingString);
+            }
+
+            return convertedFrames;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleBirthday
+{
+    class Program
+    {
+        private const int FrameRatePerSecond = 24;
+
+        static async Task Main(string[] args)
+        {
+            var frameCounter = 0;
+            var frameCount = AsciiArt.BirthdayCake.Length;
+            var drawableFrames = DrawingExtensions.ConvertFrames(AsciiArt.BirthdayCake);
+            Console.SetWindowSize(70, 40);
+            Console.Clear();
+            Console.CursorVisible = false;
+
+            do
+            {
+                DrawingExtensions.WriteFrame(drawableFrames[frameCounter]);
+
+                if (++frameCounter == frameCount)
+                {
+                    frameCounter = 0;
+                }
+
+                await Task.Delay(1000 / FrameRatePerSecond);
+            } while (true);
+        }
+    }
+}
